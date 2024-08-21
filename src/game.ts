d08bfaf59332c23ddc873ffa2c6c96e293dc8a19
HossamHamdy10241 <hossamhamdy10241@gmail.com>
2024-08-21 14:41:13 +0300

    collision detection implemented

--- src/game.ts
+++ src/game.ts
@@ -4,13 +4,14 @@ export class Game{
     w = window.innerWidth
     h = window.innerHeight
     gap=220
-    speed=3
+    speed=4
     rotSpeed=Math.PI/(this.gap/this.speed)
     key={ArrowLeft:false,ArrowRight:false}
     canvas =<HTMLCanvasElement> document.querySelector('canvas')
     ctx=this.canvas.getContext('2d') as CanvasRenderingContext2D
     player = new Player(this)
     obst :Base[] =[]
+    animationframeid:any
     constructor(){
 
         this.canvas.width=this.w
@@ -33,16 +34,63 @@ export class Game{
     obs(){
         if(this.obst.length ==0){
             for(let i =0; i<20; i++){
-                this.obst.push(new Base(this,0 + i*-this.gap));
+                this.obst.push(new Base(this,-300 + i*-this.gap));
             }
         }
 
 
     }
+    detect(){
+        let obst = this.obst
+        if(obst.length){
+            for(let i=0;i<3 && i<obst.length;i++){
+                let block = obst[i]
+                let blocdim:any={
+                    x:block.left?this.w/2-block.width/2:this.w/2+block.width/2,
+                    y:block.y+block.height/2,
+                    w:block.width,
+                    h:block.height}
+                // if(block.left){
+                //     blocdim.x=this.w/2-block.width/2
+                // }
+                // else{
+                //     blocdim.x=this.w/2+block.width/2
+
+                    
+                // }
+                let oneball = {
+                    radius:this.player.ballSize,
+                    x: this.player.x + this.player.radius * Math.cos(this.player.angel),
+                    y: this.player.y + this.player.radius * Math.sin(this.player.angel),
+
+                    
+                }
+                let otherball = {
+                    radius:this.player.ballSize,
+                    x: this.player.x + this.player.radius * Math.cos(this.player.angel+Math.PI),
+                    y: this.player.y + this.player.radius * Math.sin(this.player.angel+Math.PI),
+
+                    
+                }
+                if(oneball.radius+blocdim.w/2 >= Math.abs(blocdim.x-oneball.x) && oneball.radius+blocdim.h/2 >=Math.abs(blocdim.y-oneball.y)){
+
+                    cancelAnimationFrame(this.animationframeid)
+                    console.log('cancelled')
+                }
+                if(otherball.radius+blocdim.w/2 >= Math.abs(blocdim.x-otherball.x) && otherball.radius+blocdim.h/2 >=Math.abs(blocdim.y-otherball.y)){
+                    console.log('cancelled')
+
+
+                    cancelAnimationFrame(this.animationframeid)
+                }
+                
+            }
+        }
+
+    }
     tick(){
         this.ctx.clearRect(0,0,this.w,this.h)
         this.obs()
-        this.player.draw()
         this.player.update()
         for(let i=0;i<this.obst.length;i++){
             this.obst[i].draw()
@@ -52,8 +100,10 @@ export class Game{
                 i--
             }
         }
-
+        this.player.draw()
+        
         // this.obst.draw()        // this.player.update()
-        requestAnimationFrame(()=>this.tick())
+        this.animationframeid = requestAnimationFrame(()=>this.tick())
+        this.detect() 
     }
 }
